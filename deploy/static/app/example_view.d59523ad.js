f1b889c495a949ffdc518d550752fd0fcedf7ee6
Andreas H. Kelch <ak@mausbrand.de>
2022-02-26 20:33:16 +0100

    updated current build

---
+++ deploy/static/app/example_view.d59523ad.js
@@ -0,0 +1,1 @@
+import{_ as r}from"./index.30289d12.js";import{a as o,o as a,c,t as n}from"./vendor.0ffd3825.js";const p={components:{},props:{skel:Object,structure:Object,request_status:Number},setup(e,s){return{state:o({})}}};function u(e,s,t,_,i,l){return a(),c("div",null," EXAMPLE "+n(t.skel),1)}var d=r(p,[["render",u]]);export{d as default};
